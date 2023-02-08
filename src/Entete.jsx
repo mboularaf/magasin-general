e5da1a85a8ddf49ba087472de4ad2564edcdf319
Mohamed Boularaf <e1761826@cmaisonneuve.qc.ca>
2023-02-08 15:58:46 -0500

    ajout des items dans le panier modifiés

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -1,13 +1,14 @@
 import './Entete.scss';
-function Entete() {
-
+function Entete(props) {
+    let panier = props.panier;
+    console.log("Le panier qui nous vient de Appli affiché dans Entete : ", panier);
     return(
         <header className="Entete">
         <h1>Magasin général</h1>
         <nav>
             <span>Produits</span>
             <span>À propos</span>
-            <span>Panier</span>
+            <span>Panier ({Object.values(props.panier).length})</span>
         </nav>
         </header>
 
